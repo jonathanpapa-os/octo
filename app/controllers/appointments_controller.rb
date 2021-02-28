e31cfe991fd148da0927b7b994508179ecefe849
Ronaldo Paralejas <officialrparalejas@gmail.com>
2021-02-28 17:58:43 +0800

    Added essential classes

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,42 @@
 class AppointmentsController < ApplicationController
+
+        def index
+
+        end
+
+        def show
+
+        end
+
+        def new
+
+        end
+
+        def create
+
+        end
+
+        def edit
+
+        end
+
+        def update
+
+        end
+
+
+        def destroy
+
+            if  params[:appointments].present?
+                SavedSearch.destroy(params[:appointments][:id])
+                flash[:success] = "Saved search has been deleted"
+                redirect_to :action => 'index'
+            else
+                SavedLink.destroy(params[:appointments][:id])
+                flash[:success] = "Saved link has been deleted"
+                redirect_to :action => 'index'
+            end
+
+        end
+
 end
