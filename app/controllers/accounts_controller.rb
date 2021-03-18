b83c3f82eedc644848e4eab929d528dd8cdf5001
Jonathan Papa <jonathanpapa@paralejas.com>
2021-03-18 17:19:54 +0800

    Added 'email' on account params

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -64,6 +64,6 @@ class AccountsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def account_params
-      params.require(:account).permit(:slug, :name, :description)
+      params.require(:account).permit(:slug, :name, :description, :email)
     end
 end
