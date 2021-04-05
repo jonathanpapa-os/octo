2022a94375cb637291a61d398ce8207d29d2ee61
Jonathan Papa <jonathanpapa@paralejas.com>
2021-04-05 17:20:31 +0800

    Fixed sending 'reset password instruction' * Uncommented config.mailer = 'Devise:Mailer' and commented config.mailer = 'Mailform:Base' * Set default url options in development.rb to 'admin.localhost:3000'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
 
   # Replacement of the code config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'admin.localhost:3000' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
