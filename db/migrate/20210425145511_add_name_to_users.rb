32c0011aea364b1063ff56b51ddbc7ccadae75e2
Jonathan Papa <jonathanpapa@paralejas.com>
2021-04-27 08:55:42 +0800

    Set the additional column as 'null: false'

--- db/migrate/20210425145511_add_name_to_users.rb
+++ db/migrate/20210425145511_add_name_to_users.rb
@@ -1,5 +1,5 @@
 class AddNameToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :name, :string, :after => :email
+    add_column :users, :name, :string, :after => :email, null: false
   end
 end
