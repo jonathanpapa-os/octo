72456c58b4097244eed5d0be8cb4ea9544f9942e
Jonathan Papa <jonathanpapa-os@paralejas.com>
2021-05-07 18:02:58 +0800

    Redirected the page after login depends on user role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,19 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
+
+    def redirect
+        if user_signed_in?
+            if current_user.superadmin_role == TRUE 
+                redirect_to tenants_path
+            else
+                if current_user.supervisor_role == TRUE 
+                    redirect_to appointments_path
+                else
+                redirect_to new_appointment_path
+                end
+            end
+        end
+    end
+
 end
