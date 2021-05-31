eb5c4b17808df7aa0e2198d70feb4b5409e9b6fb
Jonathan Papa <jonathanpapa-os@paralejas.com>
2021-06-01 00:28:00 +0800

    Created Users controller and modified devise users on config/route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   constraints subdomain: /.*/ do
   end
 
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+
 
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
