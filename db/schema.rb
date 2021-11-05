0dc945747b2e57427200353df26f072b11b231f3
ronaldo paralejas <officialrparalejas@gmail.com>
2021-11-05 18:00:52 +0800

    Fixed appointmnet not showing on created appointts and on db

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_05_20_042652) do
     t.string "address"
     t.string "email"
     t.string "tenant_id"
+    t.string "appointment", limit: 30
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
