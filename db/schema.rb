becfa66641c26cae6e3d8236215308db3bd1a1bf
john-at-291715466569 <jhn.knnth.prljs@gmail.com>
2022-02-27 20:37:18 +0800

    Changed UI for edit user profile picture button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_20_042652) do
+ActiveRecord::Schema.define(version: 2022_02_14_084247) do
 
   create_table "appointments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "company_name"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2021_05_20_042652) do
     t.string "address"
     t.string "email"
     t.string "tenant_id"
-    t.string "appointment", limit: 30
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -47,6 +46,7 @@ ActiveRecord::Schema.define(version: 2021_05_20_042652) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "user_img"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
