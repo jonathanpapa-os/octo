af7a290af7a9781205d546117cc5c6e04b7b4def
ronaldo paralejas <officialrparalejas@gmail.com>
2022-06-17 16:14:51 +0800

    merged working javascript to javascript/packs

--- app/javascript/packs/create_appointment.js
+++ app/javascript/packs/create_appointment.js
@@ -1,10 +1,10 @@
 var $options = $("#appointment_day > option").clone();
 $('#for_cloning_select').append($options);
 
-  var string = document.getElementById('appointment_dates').innerHTML;
-  var array = string.split(",");
+var string = document.getElementById('appointment_dates').innerHTML;
+var array = string.split(",");
 
-  $("#appointment_month").change(function(){
+$("#appointment_month").change(function(){
     var date_to_remove = [];
     var $cloned_options = $("#for_cloning_select > option").clone();
     $.each( array, function( key, value ) {
@@ -15,26 +15,41 @@ $('#for_cloning_select').append($options);
         year=fullname[0];
         day_and_time=fullname[fullname.length-1];
         for(var i=1; i < fullname.length-1; i++) {
-           month =  month +" "+ fullname[i];
-           var i = day_and_time.indexOf(' ');
-           var day = day_and_time.slice(0, i).trim();
-           var time = day_and_time.slice(i + 1, day_and_time.length).trim();
-           date_to_remove.push(month+','+day+','+time+','+year);
+            month =  month +" "+ fullname[i];
+            var i = day_and_time.indexOf(' ');
+            var day = day_and_time.slice(0, i).trim();
+            var time = day_and_time.slice(i + 1, day_and_time.length).trim();
+            date_to_remove.push(month+','+day+','+time+','+year);
+
+            $.each(date_to_remove, function( key, value ) {
+                $('#appointment_day option').remove();
+                $('#appointment_day').append($cloned_options);
+                $("#appointment_day").val('1');
+                let text = value + '';
+                const to_remove = text.split(",");
+                var months_30 = ['March', 'April', 'June', 'August', 'September', 'November'];
+                var replaced_months = to_remove[0].replace(/\s/g, '')
+                var replaced_day = to_remove[1].replace(/\s/g, '')
+                if($( "#appointment_month option:selected" ).val()== replaced_months){
+                    $("#appointment_day option[value='"+replaced_day+"']").remove();
+                }
+
+                if($( "#appointment_month option:selected" ).val() == 'February'){
+                    var list = ["31", "30", "29"];
+                    $('#appointment_day option').filter(function () {
+                        return $.inArray(this.value, list) !== -1
+                    }).remove();
+                }
+
+                if(jQuery.inArray($( "#appointment_month option:selected" ).val(), months_30) != -1) {
+                    var list = ["31"];
+                    $('#appointment_day option').filter(function () {
+                        return $.inArray(this.value, list) !== -1
+                    }).remove();
+                }
 
 
-           $.each(date_to_remove, function( key, value ) {
-             $('#appointment_day option').remove();
-             $('#appointment_day').append($cloned_options);
-             $("#appointment_day").val('1');
-             let text = value + '';
-             const to_remove = text.split(",");
-             var replaced_months = to_remove[0].replace(/\s/g, '')
-             var replaced_day = to_remove[1].replace(/\s/g, '')
-             if($( "#appointment_month option:selected" ).val()== replaced_months){
-               $("#appointment_day option[value='"+replaced_day+"']").remove();
-             }
-
-           });
+            });
         }
     });
-  });
+});
