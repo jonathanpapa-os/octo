0646175814bdfab2b858274e857b255d83cf5adb
ronaldo paralejas <officialrparalejas@gmail.com>
2022-09-02 17:01:44 +0800

    Updated main javascript with the new working in page javascript

--- app/javascript/packs/create_appointment.js
+++ app/javascript/packs/create_appointment.js
@@ -1,48 +1,50 @@
-var $options = $("#appointment_day > option").clone();
+let $options = $("#appointment_day > option").clone();
 $('#for_cloning_select').append($options);
 
-var string = document.getElementById('appointment_dates').innerHTML;
-var array = string.split(",");
+let string = document.getElementById('appointment_dates').innerHTML;
+let array = string.split(",");
 
 $("#appointment_month").change(function(){
-    var date_to_remove = [];
-    var $cloned_options = $("#for_cloning_select > option").clone();
+    let date_to_remove = [];
+    let $cloned_options = $("#for_cloning_select > option").clone();
     $.each( array, function( key, value ) {
-        var fullname = value.split("-");
-        year = '';
-        month = '';
-        day_and_time = '';
+        let fullname = value.split("-");
+        let year;
+        let day_and_time;
         year=fullname[0];
         day_and_time=fullname[fullname.length-1];
-        for(var i=1; i < fullname.length-1; i++) {
-            month =  month +" "+ fullname[i];
-            var i = day_and_time.indexOf(' ');
-            var day = day_and_time.slice(0, i).trim();
-            var time = day_and_time.slice(i + 1, day_and_time.length).trim();
+        for(let i=1; i < fullname.length-1; i++) {
+            let month = month +" "+ fullname[i];
+            let i = day_and_time.indexOf(' ');
+            let day = day_and_time.slice(0, i).trim();
+            let time = day_and_time.slice(i + 1, day_and_time.length).trim();
             date_to_remove.push(month+','+day+','+time+','+year);
+            let appointment_options = $('#appointment_day option');
+            let appointment_options_selected = $('#appointment_day option:selected');
+
 
             $.each(date_to_remove, function( key, value ) {
-                $('#appointment_day option').remove();
+                appointment_options.remove();
                 $('#appointment_day').append($cloned_options);
                 $("#appointment_day").val('1');
                 let text = value + '';
                 const to_remove = text.split(",");
-                var months_30 = ['March', 'April', 'June', 'August', 'September', 'November'];
-                var replaced_months = to_remove[0].replace(/\s/g, '')
-                var replaced_day = to_remove[1].replace(/\s/g, '')
-                if($( "#appointment_month option:selected" ).val()== replaced_months){
+                let months_30 = ['March', 'April', 'June', 'August', 'September', 'November'];
+                let replaced_months = to_remove[0].replace(/\s/g, '')
+                let replaced_day = to_remove[1].replace(/\s/g, '')
+                if(appointment_options_selected.val()=== replaced_months){
                     $("#appointment_day option[value='"+replaced_day+"']").remove();
                 }
 
-                if($( "#appointment_month option:selected" ).val() == 'February'){
-                    var list = ["31", "30", "29"];
+                if(appointment_options_selected.val() === 'February'){
+                    let list = ["31", "30", "29"];
                     $('#appointment_day option').filter(function () {
                         return $.inArray(this.value, list) !== -1
                     }).remove();
                 }
 
-                if(jQuery.inArray($( "#appointment_month option:selected" ).val(), months_30) != -1) {
-                    var list = ["31"];
+                if(jQuery.inArray($( "#appointment_month option:selected" ).val(), months_30) !== -1) {
+                    let list = ["31"];
                     $('#appointment_day option').filter(function () {
                         return $.inArray(this.value, list) !== -1
                     }).remove();
