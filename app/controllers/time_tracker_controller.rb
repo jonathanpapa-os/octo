aa89c77eeb29f4573646296f4b1dc065b48e7d62
john-at-291715466569 <jhn.knnth.prljs@gmail.com>
2022-11-24 14:08:54 +0800

    Added time tracker controller for time tracking

---
+++ app/controllers/time_tracker_controller.rb
@@ -0,0 +1,2 @@
+class TimeTrackerController < ApplicationController
+end
---
+++ test/controllers/time_tracker_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class TimeTrackerControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
